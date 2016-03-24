9fc7e5a8489100af928f17ec5f3d8e6fb3afd59a
Kim Sarabia <kimberly.sarabia@gmail.com>
2016-03-24 10:39:30 -0700

    writing functionality in progress

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,19 +3,86 @@
 var app = angular.module("petfinder");
 
 app.controller("dashCtrl", function($scope, $state, PetService, OwnerService) {
-    console.log("dash works");
+    console.log("dashCtrl works");
+    angular.element('#price').trigger('change');
+    angular.element('#petAge').trigger('change');
+    angular.element('#petName').trigger('change');
+    angular.element('#petSpecies').trigger('change');
+    angular.element('#petBreed').trigger('change');
+    angular.element('#photoUrl').trigger('change');
+    PetService.getAll();
+    $scope.modShow = false;
+    $scope.allShow = true;
 
-    PetService.getUpcoming()
-    .then(function(res){
-      $scope.pets = res.data.map(function(pet){
-        pet.time = new Date(pet.time);
-        return pet;
-      });
-      debugger;
-    }, function(err){
-      console.error('err:' err);
-    })
+    $scope.$watch(function() {
+        return PetService.allPets;
+    }, function(curVal, preVal) {
+        console.log(curVal);
+        $scope.allPets = curVal;
+    });
+
+    $scope.deletePet = function(input) {
+      console.log(input.pet);
+      PetService.deletePet(input.pet);
+      PetService.getAll();
+    }
+
+    $scope.modifyPet = function(input){
+      $scope.modshow = true;
+      $scope.allShow = false;
+      console.log(input);
+      $scope.update = input.pet;
+    }
+
+    $scope.deletePet = function(input) {
+        console.log(input.pet);
+        PetService.deletePet(input.pet);
+        PetService.getAll();
+    }
+
+    $scope.modifyQ = function(input) {
+        $scope.modShow = true;
+        $scope.allShow = false;
+        console.log(input);
+        $scope.update = input.quest;
+
+    }
+
+    $scope.updateQ = function(input) {
+        console.log(input);
+        console.log(input.update.id);
+        flashService.update(input.update);
+        flashService.getAll();
+    }
+
+    $scope.quitUpdate = function() {
+        $scope.addShow = false;
+        $scope.modShow = false;
+        $scope.allShow = true;
+    }
+
+    $scope.addNewQuest = function() {
+        $scope.addShow = true;
+        $scope.modShow = false;
+        $scope.allShow = false;
+        console.log($scope.newQ);
+    }
+
+    $scope.addNewQ = function(input) {
+        flashService.create(input);
+        flashService.getAll();
+    }
+
+    $scope.quitAdd = function() {
+        $scope.addShow = false;
+        $scope.modShow = false;
+        $scope.allShow = true;
+        $scope.newQ = {};
+    }
+
 });
+
+
 
 app.controller("ownersCtrl", function($scope, $state, petService, ownerService) {
     console.log("ownersCtrl works");
@@ -23,5 +90,28 @@ app.controller("ownersCtrl", function($scope, $state, petService, ownerService) 
 
 app.controller("petsCtrl", function($scope, $state, petService, ownerService) {
     console.log("petsCtrl works");
+        PetService.getUpcoming()
+        .then(function(res){
+          $scope.pets = res.data.map(function(pet){
+            pet.time = new Date(pet.time);
+            return pet;
+          });
+          debugger;
+        }, function(err){
+          console.error('err:' err);
+        });
 
-});
+        $scope.getPets = function($this) {
+          console.log($this.item);
+          $scope.petShow
+        }
+
+        $scope.$watch(function() {
+          return PetService.curPetList;
+      }, function(curVal, preVal) {
+
+          $scope.numPets = PetService.numPets;
+          $scope.petsList = curVal;
+          $scope.pet = curVal[0];
+      });
+    });
--- public/js/services.js
+++ public/js/services.js
@@ -2,10 +2,29 @@
 
 var app = angular.module("petfinder");
 
-app.service('petfinder', function($http){
+app.service('petService', function($http){
+  this.curPet = [];
+  this.curPetList = [];
+  this.numPets=0;
 
-  this.getAll = function() {
-    return $http.get('/pets');
+  this.getAll = () => {
+     $http.get('/pets').then(res=> {
+       this.allPets = res.data;
+     }, err => {
+       if (err) {
+         console.log(err);
+       }
+     });
+  };
+
+  this.getById = (id) => {
+      $http.get(`/pets/${id}`).then(res => {
+        this.curPet = res.data;
+      }, err => {
+          if(err) {
+          console.log(err);
+        }
+      });
   };
 
   this.getUpcoming = function(){
@@ -24,7 +43,40 @@ app.service('petfinder', function($http){
     return $http.put(`/pets/${pet._id}/adopt`);
   };
 
+  this.deletePet = function(pet){
+
+  };
   this.addOwner = function(petId, ownerId){
     return $http.put(`/pets/${petId}/addOwner/${ownerId}`);
   };
+
+
 })
+
+app.service('ownerService', function($http) {
+    this.getAll = function() {
+        console.log("getAll");
+        return $http.get('/owners');
+    };
+
+    this.getById = function(id) {
+        return $http.get(`/owners/${id}`);
+    };
+
+    this.getByName = function(name) {
+        return $http.get(`/owners/owner/${name}`);
+    };
+
+    this.create = function(user) {
+        return $http.post('/users', user);
+    };
+
+    this.delete = function(user) {
+        return $http.delete(`/users/${user.id}`);
+    };
+
+    this.update = function(editUser) {
+        return $http.put('/users', editUser);
+    };
+
+});
